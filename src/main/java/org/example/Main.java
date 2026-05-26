86b7210857f5921d316ca8bc32dfeb04abc74f8a
Eugeniu Morarescu <eugeniu.morarescu@endava.com>
2026-05-26 12:22:33 +0300

    Testing STUB and GithubActions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,5 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         SpringApplication.run(Main.class, args);
-
     }
 }
