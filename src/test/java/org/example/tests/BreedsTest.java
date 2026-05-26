bf67b3fe0413c1f6596cf3129a7302552c4aa474
Eugeniu Morarescu <eugeniu.morarescu@endava.com>
2026-05-26 12:22:33 +0300

    Testing STUB and GithubActions

--- src/test/java/org/example/tests/BreedsTest.java
+++ src/test/java/org/example/tests/BreedsTest.java
@@ -2,8 +2,8 @@ package org.example.tests;
 
 import io.restassured.response.Response;
 import org.example.api.service.BreedsApiService;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openapitools.client.model.BreedsResponse;
 
 public class BreedsTest {
 
@@ -12,16 +12,21 @@ public class BreedsTest {
         BreedsApiService breedsApiService = new BreedsApiService();
 
         Response breedsResponse = breedsApiService.getBreed("dd9362cc-52e0-462d-b856-fccdcf24b140");
+
         assert breedsResponse.getBody().prettyPrint().contains("dd9362cc-52e0-462d-b856-fccdcf24b140");
 
     }
 
     @Test
+    @Tag("valera")
     void returnBreeds() {
         BreedsApiService breedsApiService = new BreedsApiService();
 
         Response breedsResponse = breedsApiService.getBreeds(1, 5);
-        assert breedsResponse.getBody().as(BreedsResponse.class).getData().size() == 5;
+//        assert breedsResponse.getBody().as(BreedsResponse.class).getData().size() == 5;
+        assert breedsResponse.getBody().prettyPrint().contains("The Affenpinscher is a small");
 
     }
+
+
 }
